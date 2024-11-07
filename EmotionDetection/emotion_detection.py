87678cc570cb3cdd1fb43eea769eebab9f4cb922
cots-mb <otsubo@meta-blank.com>
2024-11-07 17:13:50 +0000

    Create emotion_detection.py

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,35 @@
+import requests
+
+def emotion_detector(text_to_analyze):
+    
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    
+    
+    headers = {
+        "grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"
+    }
+    
+    
+    payload = {
+        "raw_document": {
+            "text": text_to_analyze
+        }
+    }
+    
+    
+    response = requests.post(url, headers=headers, json=payload)
+     if response.status_code == 200:
+
+        data = dict(json.loads(response.text))
+        emoPre = dict(data["emotionPredictions"][0])
+        emo= emoPre.get('emotion')
+        dominant_emo =  max(emo, key=emo.get)
+        emo['dominat_emotion'] = dominant_emo
+
+        print("\n".join("{}\t{}".format(k, v) for k, v in emo.items()))
+       
+        return dominant_emo
+
+    else:
+        
+        return response.raise_for_status()
