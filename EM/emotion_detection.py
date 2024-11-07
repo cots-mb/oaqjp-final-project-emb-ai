161aedb15ce9168d1f7fdc463c18b8fbc5566b13
cots-mb <otsubo@meta-blank.com>
2024-11-07 08:19:04 +0000

    Update emotion_detection.py

--- EM/emotion_detection.py
+++ EM/emotion_detection.py
@@ -18,13 +18,17 @@ def emotion_detector(text_to_analyze):
     
     
     response = requests.post(url, headers=headers, json=payload)
-    response = requests.post(url, headers=headers, json=payload)
-    data = dict(json.loads(response.text))
-    emo = dict(data["emotionPredictions"][0])
-    
-    if response.status_code == 200:
-        
-        return emo.get('emotion')
+     if response.status_code == 200:
+
+        data = dict(json.loads(response.text))
+        emoPre = dict(data["emotionPredictions"][0])
+        emo= emoPre.get('emotion')
+        dominant_emo =  max(emo, key=emo.get)
+        emo['dominat_emotion'] = dominant_emo
+
+        print("\n".join("{}\t{}".format(k, v) for k, v in emo.items()))
+       
+        #return emo
 
     else:
         
